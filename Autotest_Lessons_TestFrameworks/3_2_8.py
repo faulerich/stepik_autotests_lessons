cd4c5a270fa25e57db40354f351df8306015d22d
bond <faulerich@yandex.ru>
2024-02-09 13:04:26 +0300

    global_commit

--- Autotest_Lessons_TestFrameworks/3_2_8.py
+++ Autotest_Lessons_TestFrameworks/3_2_8.py
@@ -4,3 +4,5 @@
 def test_input_text(expected_result, actual_result):
     # ваша реализация, напишите assert и сообщение об ошибке
     assert (expected_result == actual_result), f"expected {expected_result}, got {actual_result}"
+
+# test
