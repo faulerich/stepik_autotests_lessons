8c3f5e9bc8358f6b8bc8634fdbedfeb959f054d5
faulerich <faulerich@yandex.ru>
2024-02-15 15:47:43 +0300

    added solving tasks 3_5_6, 3_5_7

---
+++ Autotest_Lessons_TestFrameworks/3_5_6.py
@@ -0,0 +1,20 @@
+
+# пропуск тестов
+
+import pytest
+
+# параметр Strict в случае неожиданного прохождения теста, помеченного как xfail,
+# отметит в отчете этот тест как упавший
+@pytest.mark.xfail(strict=True)
+def test_succeed():
+    assert True
+
+
+@pytest.mark.xfail
+def test_not_succeed():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped():
+    assert False
