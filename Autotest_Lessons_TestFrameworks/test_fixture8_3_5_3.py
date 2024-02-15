6a65366e350e55cd913c5d43521b01bae04c3dc3
faulerich <faulerich@yandex.ru>
2024-02-15 10:10:53 +0300

    edited pytest.ini edited 3_5_2 added 3_5_3

--- Autotest_Lessons_TestFrameworks/test_fixture8_3_5_3.py
+++ Autotest_Lessons_TestFrameworks/test_fixture8_3_5_3.py
@@ -1,5 +1,5 @@
 
-# Маркировка тестов часть 1 (запуск определенного типа тестов)
+# Маркировка тестов часть 2 (запуск для определенной ОС)
 
 import pytest
 from selenium import webdriver
@@ -17,14 +17,15 @@ def browser():
     browser.quit()
 
 
-class TestMainPage1():
+class TestMainPage1:
 
     @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-    @pytest.mark.regression
+    @pytest.mark.smoke
+    @pytest.mark.win10
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
