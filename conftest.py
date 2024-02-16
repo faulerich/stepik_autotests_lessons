1cef05cca884e69addd20ba1e5ed19939d9b0f37
faulerich <faulerich@yandex.ru>
2024-02-16 17:46:56 +0300

    added 3_6_7 updated conftest.py

--- conftest.py
+++ conftest.py
@@ -1,15 +1,26 @@
 
-# тут хранится фикстура browser
 
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+
+# настройка опции выбора браузера (по умолчанию Chrome)
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="firefox",
+                     help="Choose browser: chrome or firefox")
+
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
